d1e4b6f0f3d7ebd7d7ada4548b8fe15508f556bb
jose rodriguez <jose.rodriguez.gomez.1992@gmail.com>
2024-07-19 08:07:05 -0400

    optimiza batch input

--- app/routes/batch_input.py
+++ app/routes/batch_input.py
@@ -1,3 +1,81 @@
+# from fastapi import APIRouter, HTTPException, File, UploadFile
+# from ..models import Product, ProductIn
+# from ..database import db
+# from bson import ObjectId
+# import pandas as pd
+# from io import BytesIO
+# from datetime import datetime
+# from .products import update_dup_values
+
+# router = APIRouter()
+# product_collection = db.products
+
+# @router.post("/uploadExcelFile/")
+# async def upload_excel(file: UploadFile = File(...)):
+#     last_product = product_collection.find_one(sort=[("idChaleco", -1)])
+#     if last_product is None:
+#         last_id = 0
+#     else:
+#         last_id = last_product["idChaleco"]
+        
+#     DTYPES = {
+#         "idChaleco": int,
+#         "lote": str,
+#         "serie": str,
+#         "idic": str,
+#         "polizaNombre": str,
+#         "vencimientoPoliza": str,
+#         "vencimientoPanel": str,
+#         "modelo": str,
+#         "status": str,
+#         "talla": str,
+#         "precio": float,
+#         "cliente": str,
+#         "vendedor": str,
+#         "factura": str,
+#         "gd": str,
+#         "fechaVenta": str,
+#         "comentarios": str,
+#         "createdAt": str,
+#         "createdBy": str,
+#         "updatedAt": str,
+#         "updatedBy": str,
+#         "vencimiento_funda": str,
+#         "precio_funda": float,
+#         "dup": str
+#     }
+    
+#     if file.filename.endswith('.xlsx') or file.filename.endswith('.xls'):
+#         # Leer el contenido del archivo en un DataFrame
+#         contents = await file.read()
+#         df = pd.read_excel(BytesIO(contents),sheet_name="batch_input", dtype=DTYPES)
+
+#         # filename = '/Users/joserodriguez/Desktop/carga_masiva_armor_vest.xlsx'  # Ruta absoluta del archivo
+#         # df = pd.read_excel(filename, sheet_name="batch_input", dtype=DTYPES)
+        
+#         # Convertir las fechas al formato datetime
+#         date_columns = ["vencimientoPoliza", "vencimientoPanel", "fechaVenta", "createdAt", "updatedAt", "vencimiento_funda"]
+#         for col in date_columns:
+#             df[col] = pd.to_datetime(df[col], errors='coerce')  # 'coerce' convierte errores en NaT (Not a Time)
+        
+#         df = df.fillna(0)
+#         df = df[df["talla"] != "0"]
+#         df_json = df.to_dict(orient="records")
+        
+#         # Convertir los datos a formato JSON compatible con MongoDB
+#         for record in df_json:
+#             for field in date_columns:
+#                 if isinstance(record[field], datetime):
+#                     record[field] = record[field].isoformat()
+        
+#         # Insertar los datos en la base de datos
+#         product_collection.insert_many(df_json)
+        
+#         await update_dup_values()
+
+#         return {"result": "Archivo procesado y datos insertados con éxito"}
+
+
 from fastapi import APIRouter, HTTPException, File, UploadFile
 from ..models import Product, ProductIn
 from ..database import db
@@ -48,33 +126,30 @@ async def upload_excel(file: UploadFile = File(...)):
     if file.filename.endswith('.xlsx') or file.filename.endswith('.xls'):
         # Leer el contenido del archivo en un DataFrame
         contents = await file.read()
-        df = pd.read_excel(BytesIO(contents),sheet_name="batch_input", dtype=DTYPES)
+        df = pd.read_excel(BytesIO(contents), sheet_name="batch_input", dtype=DTYPES)
 
-        # filename = '/Users/joserodriguez/Desktop/carga_masiva_armor_vest.xlsx'  # Ruta absoluta del archivo
-        # df = pd.read_excel(filename, sheet_name="batch_input", dtype=DTYPES)
-        
         # Convertir las fechas al formato datetime
         date_columns = ["vencimientoPoliza", "vencimientoPanel", "fechaVenta", "createdAt", "updatedAt", "vencimiento_funda"]
         for col in date_columns:
-            df[col] = pd.to_datetime(df[col], errors='coerce')  # 'coerce' convierte errores en NaT (Not a Time)
+            df[col] = pd.to_datetime(df[col], errors='coerce')
         
         df = df.fillna(0)
         df = df[df["talla"] != "0"]
-        df_json = df.to_dict(orient="records")
         
-        # Convertir los datos a formato JSON compatible con MongoDB
-        for record in df_json:
-            for field in date_columns:
-                if isinstance(record[field], datetime):
-                    record[field] = record[field].isoformat()
-        
-        # Insertar los datos en la base de datos
-        product_collection.insert_many(df_json)
-        
+        # Inserción por lotes
+        batch_size = 1000
+        records = df.to_dict(orient="records")
+        for i in range(0, len(records), batch_size):
+            batch = records[i:i + batch_size]
+            for record in batch:
+                for field in date_columns:
+                    if isinstance(record[field], datetime):
+                        record[field] = record[field].isoformat()
+            product_collection.insert_many(batch)
+
         await update_dup_values()
 
         return {"result": "Archivo procesado y datos insertados con éxito"}
-
 
 
 @router.get("/deletePoliza/{poliza_nombre}")
